f3fec4bd8cdb395f2b75049c28532e31b0d0fb08
Rosalin <aybarrosalin@gmail.com>
2026-03-22 01:08:20 -0400

    cliente

---
+++ src/main/java/com/example/chocolateria/application/registrarClienteApplication.java
@@ -0,0 +1,26 @@
+package com.example.chocolateria.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class registrarClienteApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/vistas/vistaRegistroCliente.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Registro Cliente");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
